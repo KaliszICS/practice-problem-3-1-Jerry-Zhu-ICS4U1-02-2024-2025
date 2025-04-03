757a4d203388d15c826b3826f0c3bea384253d7f
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-04-03 04:12:05 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,26 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int num){
+		int sum = 0;
+		if (num < 0){
+			num = num * -1;
+		}
+if (num < 10 ){
+	sum += num % 10;
+	return sum;
+}
+return sumOfDigits(num/10) + sumOfDigits(num%10);
+}
 
+public static int factorial(int num){
+	if(num==0){
+		return 1;
+	}
+	if(num < 0){
+		return 0;
+	}
+
+	return factorial(num-1) * num; 
+}
 }
